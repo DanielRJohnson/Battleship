981b19703f79f9cfa1a7f33a7315cc4adb34d957
Daniel Johnson <danielrjohnson00@gmail.com>
2020-09-08 19:55:44 -0500

    Cleaned code and added ship preview

---
+++ constants.py
@@ -0,0 +1,10 @@
+import string 
+
+SQUARE_SIZE = 40
+NUM_ROWS = 20
+WIN_X, WIN_Y = SQUARE_SIZE * NUM_ROWS, SQUARE_SIZE * NUM_ROWS
+WHITE = [255,255,255]
+BLACK = [0,0,0]
+RED = [255,0,0]
+BLUE = [0,0,255]
+Alpha = list(string.ascii_uppercase) #list of alphabet. string lib needed
